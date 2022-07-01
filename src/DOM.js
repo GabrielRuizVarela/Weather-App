ce691695952dde38eb4d3ac8ac41bf04755f4793
GabrielRuizVarela <gabrielruizvarela@gmail.com>
2022-07-01 00:41:35 -0300

    Improved background image

--- src/DOM.js
+++ src/DOM.js
@@ -15,6 +15,7 @@ const generateCards = () => {
 generateCards();
 
 function manageTheme({ dt, sunrise, sunset }) {
+  // const img = document.querySelector('img');
   if (dt <= sunrise || dt >= sunset) {
     document.documentElement.classList.remove('light');
     document.documentElement.classList.add('dark');
